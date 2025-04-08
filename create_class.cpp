1813662f7c8e9ea0503f895a0a47b880461e3cf1
WizzOFF <maher_off@proton.me>
2025-04-08 13:53:29 +0200

    create_class.cpp: implement the methods

--- create_class.cpp
+++ create_class.cpp
@@ -10,4 +10,30 @@ class Personnage
     int m_mana;
     string m_nomArme;
     int m_degatsArme;
+
+    / Méthodes
+    void recevoirDegats(int nbDegats)
+    {
+
+    }
+
+    void attaquer(Personnage &cible)
+    {
+
+    }
+
+    void boirePotionDeVie(int quantitePotion)
+    {
+
+    }
+
+    void changerArme(string nomNouvelleArme, int degatsNouvelleArme)
+    {
+
+    }
+
+    bool estVivant()
+    {
+
+    }
 }; // N'oubliez pas le point-virgule à la fin !
