90ea09873e80d241f0b597d1f3b7539190f72e55
WizzOFF <maher_off@proton.me>
2025-04-08 13:51:26 +0200

    create_class.cpp: implement the attributes

--- create_class.cpp
+++ create_class.cpp
@@ -2,8 +2,12 @@
 #include <iostream>
  
  using namespace std;
- 
+
 class Personnage
 {
-    
+    // Attributs
+    int m_vie;
+    int m_mana;
+    string m_nomArme;
+    int m_degatsArme;
 }; // N'oubliez pas le point-virgule à la fin !
